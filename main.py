933a44e390b1336e019c899147ad03293ef5824a
BNganB <64651152+BNganB@users.noreply.github.com>
2023-07-17 16:15:30 +0100

    Selenium w/ Manual Captcha solving

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from bs4 import *
-import requests
 import re
 import numpy as np
-import time 
-import webbrowser
+import time
 
-q_term = input("Please input your search term\n")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-page = requests.get(f"https://scholar.google.com/scholar?q{q_term}&hl=en&num=20&as_sdt=0,5")
-soup = BeautifulSoup(page.content, "html.parser")
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+
+q_term = input("Please input your search term:\n")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+
+driver.get(f"https://scholar.google.com/scholar?start=0&q={q_term}&hl=en&num=20&as_sdt=0,5")
+
+while "Our systems have detected unusual traffic from your computer network." in driver.page_source:
+    time.sleep(1)
+
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
 pretty_soup = soup.prettify()
     
 
@@ -20,10 +33,10 @@ for tag in paper_tag:
 for i in range(len(paper_names)):
     paper_names[i] = paper_names[i].replace("[HTML][HTML] ", "")
 
+
 #get citation numbers
-cite_tag = soup.find_all(lambda tag: tag.name == 'a')
-cite_count = []
-pattern = re.compile("\">Cited by [0-9]+</a>")
+pattern = r'Cited by (\d+)'
+cite_count = re.findall(pattern, pretty_soup)
 
 
 #get paper links
@@ -32,19 +45,19 @@ link_array = []
 for i in range(len(link_tag)):
     link_array.append(link_tag[i].a["href"])
 
-#testing cite output
-print (cite_tag)
-print(type(cite_count))
+driver.quit()
 
-merged_array = np.column_stack([paper_names, cite_count])
-merged_array_sorted = np.sort(merged_array, axis= -1)
+
+merged_array = np.column_stack([cite_count, paper_names, link_array])
+sort_indices = np.argsort(merged_array[:, 0])[::-1]
+merged_array_sorted = merged_array[sort_indices]
+
 
 j = 0
 
 for i in merged_array_sorted:
-    print("Index: {}\tPaper: {}\tCitations: {}".format(j, merged_array_sorted[j, 0], merged_array_sorted[j, 1]))
+    print("Index: {} | Paper: {} | Citations: {} | Link: {}".format(j, merged_array_sorted[j, 1], merged_array_sorted[j, 0], merged_array_sorted[j, 2]))
     j = j + 1
-
 
 
 
